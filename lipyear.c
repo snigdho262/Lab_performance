d3818b99af2976e6034d5652bfe1852324754ed3
snigdho262 <snigdhoroy262@gmail.com>
2026-07-21 02:34:43 +0600

    add

--- lipyear.c
+++ lipyear.c
@@ -33,3 +33,98 @@ int main(){
     return 0;
 }
 
+
+// function
+#include <stdio.h>
+
+long long factorial(int n) {
+    long long result = 1;
+    int i = 1;
+
+    while (i <= n) {
+        result = result * i;
+        i = i + 1;
+    }
+
+    return result;
+}
+
+int main() {
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Error: Factorial is not defined for negative numbers.\n");
+    } else {
+        long long ans = factorial(n);
+        printf("Factorial = %lld\n", ans);
+    }
+
+    return 0;
+}
+
+// file
+#include <stdio.h>
+
+struct Student {
+    int id;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student list[5];
+    int i;
+
+    // ---- Take input for 5 students ----
+    i = 0;
+    while (i < 5) {
+        printf("\nStudent %d\n", i + 1);
+
+        printf("Enter ID: ");
+        scanf("%d", &list[i].id);
+
+        printf("Enter Name: ");
+        scanf("%s", list[i].name);
+
+        printf("Enter Marks: ");
+        scanf("%f", &list[i].marks);
+
+        i = i + 1;
+    }
+
+    FILE *fp = fopen("students.txt", "w");
+
+    i = 0;
+    while (i < 5) {
+        fprintf(fp, "%d %s %.2f\n", list[i].id, list[i].name, list[i].marks);
+        i = i + 1;
+    }
+
+    fclose(fp);
+
+    // ---- Read all students back from file ----
+    FILE *fp2 = fopen("students.txt", "r");
+
+    i = 0;
+    while (i < 5) {
+        fscanf(fp2, "%d %s %f", &list[i].id, list[i].name, &list[i].marks);
+        i = i + 1;
+    }
+
+    fclose(fp2);
+
+    // ---- Display as a table ----
+    printf("\nID\tName\tMarks\n");
+    printf("--------------------\n");
+
+    i = 0;
+    while (i < 5) {
+        printf("%d\t%s\t%.2f\n", list[i].id, list[i].name, list[i].marks);
+        i = i + 1;
+    }
+
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,18 +1,28 @@
-#include<stdio.h>
-// int main(){
-//     int year;
-    
-//     printf("Enter a year: ");
-//     scanf("%d", &year);
+#include <stdio.h>
 
-//     if(year%4 == 0){
-//         printf("Lipyear");
-//     } else if (year%400 == 0)
-//     {
-//         printf("Not lipyear");
-//     } else
-//     {
-//         printf("Invalid year.");
+// long long factorial(int n) {
+//     long long result = 1;
+//     int i = 1;
+
+//     while (i <= n) {
+//         result = result * i;
+//         i = i + 1;
+//     }
+
+//     return result;
+// }
+
+// int main() {
+//     int n;
+
+//     printf("Enter a positive integer: ");
+//     scanf("%d", &n);
+
+//     if (n < 0) {
+//         printf("Error: Factorial is not defined for negative numbers.\n");
+//     } else {
+//         long long ans = factorial(n);
+//         printf("Factorial = %lld\n", ans);
 //     }
 
 //     return 0;
